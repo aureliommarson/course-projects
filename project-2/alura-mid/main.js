715e0fea988fbde275c4fcb785d5b5cbd56eaf48
Aurélio Marson <aureliommarson@gmail.com>
2025-11-19 16:11:07 -0300

    feat: add Alura MIDI project with HTML, CSS, and JavaScript for a virtual keyboard, including audio functionality and styling

---
+++ project-2/alura-mid/main.js
@@ -0,0 +1,32 @@
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    console.log("Elemento não encontrado/ou seletor.");
+  }
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for (let cont = 0; cont < listaDeTeclas.length; cont++) {
+  const tecla = listaDeTeclas[cont];
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
+}
